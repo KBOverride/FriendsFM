ceba60aacb45402a63a95624cf92d6923a97b44f
KBOverride <kabirsingh102@hotmail.com>
2017-09-20 22:29:46 -0400

    Figuring out ways to search for lyrics in the srt files

--- FriendsFM.py
+++ FriendsFM.py
@@ -23,15 +23,15 @@ import json
 s=json.dumps(book)
 
 
-subs = pysrt.open('D://Users/Kabir/FriendsFM/Friends_Subs/Friends.S01E01.720p.BluRay.x264-PSYCHD.srt')
+#subs = pysrt.open('D://Users/Kabir/FriendsFM/Friends_Subs/Friends.S01E01.720p.BluRay.x264-PSYCHD.srt')
 
 
 # printing the subs
-i = 0
-while i < len(subs):
-	test_sub = subs[i]
-	# print(subs.text)
-	i +=1
+# i = 0
+# while i < len(subs):
+# 	test_sub = subs[i]
+#	print(subs.text)
+# 	i +=1
 
 #print(test_sub.start.minutes,":",test_sub.start.seconds)
 #print(test_sub.start.seconds)
@@ -41,14 +41,47 @@ while i < len(subs):
 #print(test_sub.end.seconds)
 #print(test_sub.end.milliseconds)
 
+
+i = 0
+j = None
 lyrics = (PyLyrics.getLyrics('Survivor','Eye of the Tiger'))
 
-print(lyrics)
+#print(lyrics)
 
 data = lyrics.split()
-
+lyricsLen = len(data)
+print(data[0])
 print(data[1])
+print(data[2])
+print(data[3])
+
 path = 'D://Users/Kabir/FriendsFM/Friends_Subs/'
 
-#for filename in os.listdir(path):
-	#print(filename)
+for i, item in enumerate(data):
+	for filename in os.listdir(path):
+		subs = pysrt.open(path + filename)
+		#print(len(subs))
+		for part in subs:
+			if data[i] in part.text:
+				i+=1
+				j = True
+				print(part.text)
+				print("\n")
+				break
+				# print(subs.text)
+		if j:
+			break
+	j = False
+
+# for filename in os.listdir(path):
+# 	subs = pysrt.open(path + filename)
+# 	#print(len(subs))
+# 	for part in subs:
+# 		if data[3] in part.text:
+# 			i+=1
+# 			j = True
+# 			print(part.text)
+# 			break
+# 			# print(subs.text)
+# 	if j:
+# 		break
