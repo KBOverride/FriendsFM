e6d7d3bddfd4d93c16d961aeeedc70945426ccfd
KBOverride <kabirsingh102@hotmail.com>
2017-09-21 19:16:57 -0400

    Updating algorithm for searching through files

--- FriendsFM.py
+++ FriendsFM.py
@@ -1,6 +1,7 @@
 import pysrt
 import os
 import glob
+import re
 from pysrt import SubRipFile
 from pysrt import SubRipItem
 from pysrt import SubRipTime
@@ -23,15 +24,18 @@ import json
 s=json.dumps(book)
 
 
-#subs = pysrt.open('D://Users/Kabir/FriendsFM/Friends_Subs/Friends.S01E01.720p.BluRay.x264-PSYCHD.srt')
+subs = pysrt.open('D://Users/Kabir/FriendsFM/Friends_Subs/Friends.S01E01.720p.BluRay.x264-PSYCHD.srt')
 
 
-# printing the subs
+#printing the subs
 # i = 0
 # while i < len(subs):
 # 	test_sub = subs[i]
-#	print(subs.text)
+# 	print(subs.index)
 # 	i +=1
+
+#for i in subs:
+	#print(i.index)
 
 #print(test_sub.start.minutes,":",test_sub.start.seconds)
 #print(test_sub.start.seconds)
@@ -43,7 +47,7 @@ s=json.dumps(book)
 
 
 i = 0
-j = None
+j = False
 lyrics = (PyLyrics.getLyrics('Survivor','Eye of the Tiger'))
 
 #print(lyrics)
@@ -54,16 +58,24 @@ print(data[0])
 print(data[1])
 print(data[2])
 print(data[3])
+print("\n")
+
+for k in range(lyricsLen):
+	data[k] = re.sub(r'[^\w\s]','',data[k])
+
+print(data)
+print("\n")
 
 path = 'D://Users/Kabir/FriendsFM/Friends_Subs/'
 
+# Searching through every srt file and matching each word from the lyrics of the song to the word in the srt file
 for i, item in enumerate(data):
 	for filename in os.listdir(path):
 		subs = pysrt.open(path + filename)
 		#print(len(subs))
 		for part in subs:
-			if data[i] in part.text:
-				i+=1
+			searchObj = re.search(r'\b' + data[i] + r'\b', part.text, re.IGNORECASE)
+			if searchObj:
 				j = True
 				print(part.text)
 				print("\n")
@@ -71,17 +83,7 @@ for i, item in enumerate(data):
 				# print(subs.text)
 		if j:
 			break
+	if j == False:
+		print(data[i])
 	j = False
-
-# for filename in os.listdir(path):
-# 	subs = pysrt.open(path + filename)
-# 	#print(len(subs))
-# 	for part in subs:
-# 		if data[3] in part.text:
-# 			i+=1
-# 			j = True
-# 			print(part.text)
-# 			break
-# 			# print(subs.text)
-# 	if j:
-# 		break
+	
