13403e59b5e84667a91443301e009cd5c3cb2afb
KBOverride <kabirsingh102@hotmail.com>
2017-09-25 00:39:48 -0400

    Continuous file searching instead of restarting
    Able to now scroll through files continously without having to restart
    from the beginning of the directory

--- FriendsFM.py
+++ FriendsFM.py
@@ -11,88 +11,85 @@ from PyLyrics import *
 
 pydub.AudioSegment.converter = r"D://FFmpeg/bin/ffmpeg.exe"
 
-book = {}
-book['kb'] = {
-	'name': 'tom',
-	'address': '123 Springfield',
-	'phone': 1234567890
-}
+# book = {}
+# book['kb'] = {
+# 	'name': 'tom',
+# 	'address': '123 Springfield',
+# 	'phone': 1234567890
+# }
 
-book['sam'] = {
-	'name': 'sam',
-	'address': '124 Springfield',
-	'phone': 5392849034
-}
+# book['sam'] = {
+# 	'name': 'sam',
+# 	'address': '124 Springfield',
+# 	'phone': 5392849034
+# }
 
-import json
-s=json.dumps(book)
-
-
-#subs = pysrt.open('D://Users/Kabir/FriendsFM/Friends_Subs/Friends.S01E01.720p.BluRay.x264-PSYCHD.srt')
-
-
-#printing the subs
-# i = 0
-# while i < len(subs):
-# 	test_sub = subs[i]
-# 	print(subs.index)
-# 	i +=1
-
-#for i in subs:
-	#print(i.index)
-
-#print(test_sub.start.minutes,":",test_sub.start.seconds)
-#print(test_sub.start.seconds)
-#print(test_sub.start.milliseconds)
-
-#print(test_sub.end.minutes)
-#print(test_sub.end.seconds)
-#print(test_sub.end.milliseconds)
-
+# import json
+# s=json.dumps(book)
 
 i = 0
 j = False
+k = 0
 Artist = 'Survivor'
 Song = 'Eye of the Tiger'
+filename = ''
 lyrics = (PyLyrics.getLyrics(Artist,Song))
 combinedAudio = AudioSegment.empty()
-
-#print(lyrics)
+firstVisitedFlag = True
 
 data = lyrics.split()
 lyricsLen = len(data)
-print(data[0])
-print(data[1])
-print(data[2])
-print(data[3])
 print("\n")
 
-for k in range(lyricsLen):
-	data[k] = re.sub(r'[^\w\s]','',data[k])
+# Removing punctuation symbols from song lyrics so it would match with the subtitles
+for i in range(lyricsLen):
+	data[i] = re.sub(r'[^\w\s]','',data[i])
 
 print(data)
 print("\n")
 
-filename = ''
-
 subs_path = 'D://Users/Kabir/FriendsFM/Friends_Subs/'
 video_path = 'D://Users/Kabir/MyVideos/FriendsSeason1-10COMPLETE720pBrRipx264[PAHE.in]/'
-audio_path = 'D://Users/Kabir/MyVideos/FriendsSeason1-10COMPLETE720pBrRipx264[PAHE.in]/FriendsAudio'
+audio_path = 'D://Users/Kabir/MyVideos/FriendsSeason1-10COMPLETE720pBrRipx264[PAHE.in]/FriendsAudio/mp3'
 export_path = 'D://Users/Kabir/FriendsFM/Friends_Audio/'
-season_path = 0
 
-# Searching through every srt file and matching each word from the lyrics of the song to the word in the srt file
+fnames = os.listdir(subs_path)
+fnamesLen = len(fnames)
+
+# In case the word is not found I replaced the audio with "something"
+# subs = pysrt.open(subs_path + 'Friends.S01E01.720p.BluRay.x264-PSYCHD.srt')
+# somethingAudio = AudioSegment.from_mp3(audio_path + '/Friends.S01E01.720p.BluRay.x264-PSYCHD.mp3')
+# t1 = subs[2].start.seconds * 1000 + subs[2].start.milliseconds
+# t2 = subs[2].end.seconds * 1000 + subs[2].end.milliseconds
+# somethingAudio = somethingAudio[t1:t2]
+# somethingAudio.export(export_path + 'something.mp3', format='mp3')
+
+#Searching through every srt file and matching each word from the lyrics of the song to the word in the srt file
 for i, item in enumerate(data):
-	for filename in os.listdir(subs_path):
-		subs = pysrt.open(subs_path + filename)
-		#print(len(subs))
+	firstVisitedFile = ''
+	while k < fnamesLen:
+		# If I went through all the files and couldn't find matching subs
+		if (firstVisitedFile == fnames[k]) and (firstVisitedFlag == False):
+			firstVisitedFlag = True
+			print(data[i])
+			print("NOT FOUND")
+			# combinedAudio = combinedAudio + somethingAudio
+			break
+		if firstVisitedFlag:
+			firstVisitedFile = fnames[k]
+			firstVisitedFlag = False
+		subs = pysrt.open(subs_path + fnames[k])
+		# Looking for first matching sub
 		for part in subs:
-			searchObj = re.search(r'\b' + data[i] + r'\b', part.text, re.IGNORECASE)
-			if searchObj:
-				j = True
-				print(part.text)
+			searchSub = re.search(r'\b' + data[i] + r'\b', part.text, re.IGNORECASE)
+			if searchSub:
 				print("\n")
+				print(data[i])
+				print(part.text)
+				j = True
+				firstVisitedFlag = True
 				# Finds the part of the subs in which the word is being said and exports it as a new audio file for Google Speech Api
+				filename = fnames[k]
 				mp3_filename = filename[0:38] + '.mp3'
 				newAudio = AudioSegment.from_mp3(audio_path + '/' + mp3_filename)
 				t1 = part.start.minutes * 60 * 1000 + part.start.seconds * 1000 + part.start.milliseconds
@@ -100,13 +97,17 @@ for i, item in enumerate(data):
 				newAudio = newAudio[t1:t2]
 				combinedAudio = combinedAudio + newAudio
 				break
-				# print(subs.text)
+		if k == fnamesLen - 1:
+			k = 0
+		else:
+			k += 1
+
 		if j:
+			j = False
 			break
-	if j == False:
-		print(data[i])
-	j = False
 
 combinedAudio.export(export_path + 'combined.mp3', format='mp3')
+
+
 
 	
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # FriendsFM
-Takes song lyrics and tries to make the song using audio from the popular sitcom "Friends"
+Takes song lyrics and makes the song using audio from the popular sitcom "Friends"
 
 ## Library and Frameworks
 - pysrt (Subtitle Parser)
@@ -7,3 +7,13 @@ Takes song lyrics and tries to make the song using audio from the popular sitcom
 - pydub (Manipulating audio)
 - Google Speech API
 
+Audio Folder is not on GitHub due to file size limits
+
+## Current Problems that Need Fixing
+- FriendsFM only analyzes Season 1-7. Need to fix Season 8-10.
+- Season 6 Episode 23 does not have subs
+- Season 8 Episode 11 cannot be converted to audio (memory error or big file size)
+- Season 9 Episode 10 cannot be converted to audio (memory error or big file size)
+- Season 9 Episode 23 Episode 24 cannot be converted (memory error)
+
+Current Runtime for "Eye of the Tiger" without taking audio to Google Speech API: **14 min 35 sec**
